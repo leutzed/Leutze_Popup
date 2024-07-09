cde797482ce55c2e8a46c2b65ccdfc2a5a3da359
Daniel Leutze <leutzed@gmail.com>
2024-07-09 19:37:55 +0000

    The action controller for rendering a list of popups

---
+++ Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace Leutze\Popup\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\View\Result\Page;
+
+class Index extends Action {
+  public function execute(): ResultInterface {
+    /** @var Page $page */
+    $page = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+
+    $page->setActiveMenu('Leutze_Popup::popup');
+    $page->addBreadcrumb(__('Popups'), __('Popups'));
+    $page->addBreadcrumb(__('Manage Popups'), __('Manage Popups'));
+    $page->getConfig()->getTitle()->prepend(__('Popup'));
+
+    return $page;
+  }
+}
